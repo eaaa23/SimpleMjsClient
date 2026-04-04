1c46338a3e45a44bf74c29e945453d47a9e341bb
eaaa23 <2694347208@qq.com>
2026-04-04 11:38:48 +0800

    Cleared Test of Default Script

--- scripts/default/__init__.py
+++ scripts/default/__init__.py
@@ -4,5 +4,5 @@ from .test2 import TestScript2
 
 NAME = "Default"
 NAME_LOCALIZED = {"EN": "Default", "CN": "内置脚本包"}
-SCRIPT_CLASSES = [KokushiScript, TestScript2]
+SCRIPT_CLASSES = [KokushiScript]
 
