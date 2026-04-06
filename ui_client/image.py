204810eae1a2896fb0ee4a2361d50a2047a5ead7
eaaa23 <2694347208@qq.com>
2026-04-06 15:05:24 +0800

    Optimized image cache (60%-70% time cutoff on each redraw)

--- ui_client/image.py
+++ ui_client/image.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import os
 import tkinter as tk
 from typing import Callable, Literal
@@ -26,29 +27,45 @@ rotation=3 -> PIL.Image.ROTATE_270
 """
 
 
+@dataclass
+class _ImageCacheKey:
+    """
+    A few signatures of transformed image to meet caches.
+    """
+    width: int
+    height: int
+    alpha: float
+
+    def __hash__(self):
+        return hash((self.width, self.height, self.alpha))
+
+
 class _Image:
     def __init__(self, path: str):
         self.path = path
         self.src: Image.Image | None = None
-        self.images: dict[tuple[int, int, float], list[Image.Image]] = {}
+        self.images: dict[_ImageCacheKey, list[ImageTk.PhotoImage]] = {}
 
-    def get(self, rotation: int, scale: float, alpha: float) -> Image.Image:
+    def get(self, rotation: int, scale: float, alpha: float) -> ImageTk.PhotoImage:
         if self.src is None:
             self.src = Image.open(self.path)
         new_width = round(self.src.width * scale)
         new_height = round(self.src.height * scale)
-        if (new_width, new_height, alpha) not in self.images:
+        key = _ImageCacheKey(new_width, new_height, alpha)
+        if key not in self.images:
             src_adjusted = self.src.resize((new_width, new_height), Image.Resampling.LANCZOS)
             if alpha != 1.0:
                 src_adjusted = src_adjusted.convert("RGBA")
                 a = round(256 * alpha)
                 src_adjusted.putdata([(r, g, b, a) for r, g, b, _ in src_adjusted.getdata()])
-            self.images[new_width, new_height, alpha] = [src_adjusted,
-                                                         src_adjusted.transpose(Image.ROTATE_90),
-                                                         src_adjusted.transpose(Image.ROTATE_180),
-                                                         src_adjusted.transpose(Image.ROTATE_270)]
 
-        return self.images[new_width, new_height, alpha][rotation]
+            images = [src_adjusted,
+                      src_adjusted.transpose(Image.ROTATE_90),
+                      src_adjusted.transpose(Image.ROTATE_180),
+                      src_adjusted.transpose(Image.ROTATE_270)]
+            self.images[key] = [ImageTk.PhotoImage(img) for img in images]
+
+        return self.images[key][rotation]
 
 
 class _ImageManager:
@@ -61,7 +78,7 @@ class _ImageManager:
                 self._src_images[filename] = _Image(os.path.join(path, filename))
 
     def __call__(self, filename: str, rotation: int = 0, scale: float = 1.0, alpha: float = 1.0) -> ImageTk.PhotoImage:
-        return ImageTk.PhotoImage(self._src_images[filename].get(rotation % 4, scale, alpha))
+        return self._src_images[filename].get(rotation % 4, scale, alpha)
 
 
 Img: _ImageManager = _ImageManager()
@@ -81,6 +98,5 @@ ANCHOR: list[Anchor] = [tk.NW, tk.SW, tk.SE, tk.NE]
 
 
 def abs_anchor(rotation: int, rel_anchor: Anchor) -> Anchor:
-    """Get the absolute anchor from relative anchor and rotation.
-    """
+    """Get the absolute anchor from relative anchor and rotation."""
     return ANCHOR[(ANCHOR.index(rel_anchor) + rotation) % 4]
