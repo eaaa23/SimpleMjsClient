96ed1ed8daeaed8089b2394dba98278d3bf15512
eaaa23 <2694347208@qq.com>
2026-04-09 11:07:23 +0800

    Pack exe ok

---
+++ pack_exe.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import zipfile
+
+import PyInstaller.__main__
+
+PyInstaller.__main__.run(["LiteSoul.py", "--onefile", "--windowed", "--ico=assets/tray.png"])
+
+shutil.move("dist/LiteSoul.exe", "LiteSoul.exe")
+
+z = zipfile.ZipFile("LiteSoul.zip", 'w')
+z.write("LiteSoul.exe")
+
+def add_folder(zipf, folder_name: str):
+    for root, dirs, files in os.walk(folder_name):
+        for file in files:
+            zipf.write(os.path.join(root, file))
+
+
+add_folder(z, "assets")
+add_folder(z, "text")
+add_folder(z, "scripts")
+os.remove("LiteSoul.exe")
+shutil.rmtree("build")
+shutil.rmtree("dist")
